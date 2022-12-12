f3d4d06216dbbaa3f03ad595091857cec586b1a2
plozanom <54781407+plozanom@users.noreply.github.com>
2022-12-11 19:40:24 -0500

    First commit

---
+++ Flet/basic_row.py
@@ -0,0 +1,14 @@
+#Creates a row 
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+    ft.Row(controls=[
+        ft.Text("A"),
+        ft.Text("B"),
+        ft.Text("C")
+    ])
+)
+
+ft.app(target=main)
